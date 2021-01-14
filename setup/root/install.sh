3d4079e84d6e69ff1bcc5b6c6456b188fb3eb0e3
Paulpoco <paulpoco@gmail.com>
2021-01-13 21:03:42 -0800

    Update install.sh

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -3,14 +3,14 @@
 # exit script if return code != 0
 set -e
 
-# define pacman packages
-pacman_packages="python2 python2-pip"
+# define pacman packages, should default to Python 3.x
+pacman_packages="python python-pip"
 
 # install pre-reqs
 pacman -Sy --needed $pacman_packages --noconfirm
-pip2 install --upgrade setuptools
-pip2 install flexget
-pip2 install transmissionrpc
+pip install --upgrade setuptools
+pip install flexget
+pip install transmissionrpc
 
 # call aor script (arch official repo)
 # source /root/aur.sh
